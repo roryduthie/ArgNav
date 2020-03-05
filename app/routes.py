061d8b644c802250b087ee029a8b60c2e67600e6
Rory Duthie <roryduthie@gmail.com>
2020-03-05 16:50:42 +0000

    changed index page

--- app/routes.py
+++ app/routes.py
@@ -7,11 +7,7 @@ from app.svg_parse import SVGParse
 @app.route('/')
 @app.route('/index')
 def index():
-    user = 'me'
-    d = {'col1': [1, 2], 'col2': [3, 4]}
-    df = pd.DataFrame(data=d)
-    items = df.to_html(classes='data', header="true")
-    return render_template('index.html', title='Home', user=user, table=[items])
+    return redirect('/form')
  
 @app.route('/form') 
 def my_form():
