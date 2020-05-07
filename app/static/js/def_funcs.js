9e2aefee26fe9227901a75988d1ac675128bfc57
Rory Duthie <roryduthie@Rorys-MBP.lan>
2020-05-07 08:21:42 +0100

    Add locution search functionality

--- app/static/js/def_funcs.js
+++ app/static/js/def_funcs.js
@@ -276,3 +276,22 @@ function uploadAIFdb() {
             }
         }
     }
+
+    function locsearchtab() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("locsearch");
+        filter = input.value.toUpperCase();
+        table = document.getElementsByClassName('locframe')[0];
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
