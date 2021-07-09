a30e2d35d6616f48f6ebd898caa4b949fc5df3b2
roryduthie <roryduthie@gmail.com>
2021-07-09 15:47:07 +0100

    Add comments to code

--- app/centrality.py
+++ app/centrality.py
@@ -8,18 +8,28 @@ import re
 
 import networkx as nx
 
-
-
 class Centrality:
+    """
+    Class to perform operations over AIF graphs.
+
+    The class pulls data from AIFdb through url creation, calls the translation of AIF json into a networkx directed graph.
+    Functions also perform operations over the networkx graphs
+
+    """
     @staticmethod
     def get_nodeset_path(nodeset_id):
-        corpus_name = 'US2016tv'
+        """
+        Function to create a nodeset path based on the nodeset id in the examples folder
+        """
         directory_path = 'examples/'
         node_path = directory_path + 'nodeset' + nodeset_id + '.json'
         return node_path
     
     @staticmethod
     def get_svg_path(nodeset_id):
+        """
+        Function to create an svg path based on the nodeset id in the examples folder
+        """
         #corpus_name = 'US2016tv'
         directory_path = 'examples/'
         node_path = directory_path + nodeset_id + '.svg'
@@ -27,7 +37,15 @@ class Centrality:
     
     @staticmethod
     def create_svg_url(nodeset_id, isMap):
+        """
+        Function to create an svg url based on the nodeset id or corpus shortname from aifdb
+        Parameters
         
+        nodeset_id: str
+        isMap: Bool
+            True if nodeset id
+            False if corpus shortname
+        """
         if isMap:
             return 'http://www.aifdb.org/diagram/svg/' + nodeset_id
         else:
@@ -37,6 +55,19 @@ class Centrality:
     @staticmethod
     def create_json_url(nodeset_id, isMap):
         
+        """
+        Function to create an AIFdb url based on the nodeset id or corpus shortname from aifdb
+
+
+        Parameters
+        ----------
+
+        nodeset_id: str
+        isMap: Bool
+            True if nodeset id
+            False if corpus shortname
+        """
+
         if isMap:
             return 'http://www.aifdb.org/json/' + nodeset_id
         else:
@@ -45,6 +76,14 @@ class Centrality:
 
     @staticmethod
     def get_graph(node_path):
+        """
+        Function to create a networkx graph from json data.
+        Calls the corpus loader class, opens the file at a path, and returns the graph.
+
+        Parameters
+        ----------
+        node_path: path to AIF json data to create networkx graph.
+        """
         corpus_loader = CorpusLoader()
         try:
             with app.open_resource(node_path) as json_data:
@@ -57,10 +96,19 @@ class Centrality:
     
     @staticmethod
     def get_graph_url(node_path):
+        """
+        Function to create a networkx graph from a url containing json data.
+        Calls the corpus loader class, opens the file at the url, and returns the graph.
+
+        Parameters
+        ----------
+        node_path: path to AIF json data to create networkx graph.
+        """
         corpus_loader = CorpusLoader()
-        print(node_path)
         try:
             jsn_string = requests.get(node_path).text
+
+            #Check for bracket to determine if string contains JSON data
             strng_ind = jsn_string.index('{')
             n_string = jsn_string[strng_ind:]
             dta = json.loads(n_string)
@@ -73,7 +121,15 @@ class Centrality:
 
     @staticmethod
     def remove_redundant_nodes(graph):
+        """
+        Function to remove all IAT nodes leaving only AIF nodes - I-nodes and S-nodes
 
+        Find the nodes,add them to a list and then remove from graph using networkx function.
+
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         node_types=nx.get_node_attributes(graph,'type')
 
         nodes_to_remove =  [x for x,y in graph.nodes(data=True) if y['type']=='TA' or y['type']=='L' or y['type']=='YA']
@@ -81,13 +137,31 @@ class Centrality:
         graph.remove_nodes_from(nodes_to_remove)
         
         return graph
+
     @staticmethod
     def remove_iso_nodes(graph):
+        """
+        Function to remove isolated nodes from the graph.
+
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         graph.remove_nodes_from(list(nx.isolates(graph)))
         return graph
         
     @staticmethod
     def get_eigen_centrality(graph):
+        """
+        Function to get centrality of I-nodes from a graph.
+
+        Function calculates eigenvector centrality for all nodes and then selects I-nodes from the graph.
+        Where Eigen vector centrality cannot converge an exception is thrown, at which point normalised degree centrality is used.
+
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         try:
             cent = nx.eigenvector_centrality_numpy(graph)
         except:
@@ -99,6 +173,14 @@ class Centrality:
         
     @staticmethod        
     def sort_by_centrality(i_nodes):
+        """
+        Function to sort I-nodes by the centrality.
+
+        Outputs a tuple of I-node ID and text.
+        Parameters
+        ----------
+        i_nodes: I_nodes extracted from the aif graph
+        """
         sorted_by_second = sorted(i_nodes, key=lambda tup: tup[1])
         ordered_ids = [(i[0],i[2]) for i in sorted_by_second]
         
@@ -106,12 +188,31 @@ class Centrality:
     
     @staticmethod
     def get_schemes(graph):
+        """
+        Function to get schemes from S-nodes.
 
+        Function is implmented for all possible S-nodes, but currently only RAs have schemes.
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         all_nodes =  [(x,y['text']) for x,y in graph.nodes(data=True) if y['type']=='RA' and y['text']!='Default Inference' or y['type']=='CA' and y['text']!='Default Conflict' or y['type']=='MA' and y['text']!='Default Rephrase']
         return all_nodes
 
     @staticmethod
     def get_ra_i_schemes_nodes(graph, scheme_ras):
+        """
+        Function to get the I-node connected to a scheme s-node.
+
+        Function is implmented for all possible S-nodes, but currently only RAs have schemes.
+        Function takes an S-node and finds all successors of the node. An S-node should only have a single outgoing edge to an I-node.
+        Therefore, the first element from the returned list is taken.
+        Outputs a tuple containing the S-node ID, s-node text I_node id and I-node text.
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        scheme_ras: all s-nodes that contain some form of scheme - i.e. not a default
+        """
         ra_tups = []
         for ra in scheme_ras:
             node_succ = list(graph.successors(ra[0]))
@@ -124,6 +225,20 @@ class Centrality:
 
     @staticmethod
     def get_all_schemes_nodes(graph, scheme_ras):
+        """
+        Function to get the I-nodes connected to a scheme s-node - both incoming and outgoing.
+
+        Function is implmented for all possible S-nodes, but currently only RAs have schemes.
+        Function takes an S-node and finds all successors of the node. An S-node should only have a single outgoing edge to an I-node.
+        Therefore, the first element from the returned list is taken.
+        Function thentakes an S-node and finds all predecessors of the node. An S-node may have several incoming I-nodes. If an IAT graph it will also have connections to a YA node which can be ignored.
+        Therefore, the list is iterated.
+        Outputs a tuple containing the S-node ID, s-node text I_node id and I-node text.
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        scheme_ras: all s-nodes that contain some form of scheme - i.e. not a default
+        """
         ra_tups = []
         for ra in scheme_ras:
             node_succ = list(graph.successors(ra[0]))
@@ -147,21 +262,49 @@ class Centrality:
 
     @staticmethod
     def list_nodes(graph):
+        """
+        Function to get all nodes from a graph in a list.
+
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         return list(graph)
     
     @staticmethod
     def get_s_node_list(graph):
+        """
+        Function to get all S-nodes from a graph in a list.
+
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         s_nodes =  [x for x,y in graph.nodes(data=True) if y['type']=='MA' or y['type']=='RA' or y['type']=='CA' or y['type']=='PA']
         return s_nodes
     
 
     @staticmethod
     def get_l_node_list(graph):
+        """
+        Function to get all l-nodes from a graph in a list.
+
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         l_nodes =  [(x,y['text']) for x,y in graph.nodes(data=True) if y['type']=='L']
         return l_nodes
 
     @staticmethod
     def get_divergent_nodes(graph):
+        """
+        Function to get all I-nodes that have multiple outgoing edges.
+
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         list_of_nodes = []
     
         for v in list(graph.nodes):
@@ -173,6 +316,19 @@ class Centrality:
     
     @staticmethod
     def get_loc_prop_pair(graph):
+        """
+        Function to get all L-nodes that are connected to an I-node through a YA-node.
+
+        First find all predeccessors of an I-node, iterate these nodes to ensure they are YAs. Agreeing YAs have a different format so ignore these.
+
+        From the YAs find all predeccessors, get the corresponding L-nodes for the I-node.
+
+        Return a locution - I-node ID tuple list.
+
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         i_node_ids =  [x for x,y in graph.nodes(data=True) if y['type']=='I']
         locution_prop_pair = []
         for node_id in i_node_ids:
@@ -193,6 +349,19 @@ class Centrality:
 
     @staticmethod
     def get_child_edges(graph):
+        """
+        Function to get all edges connected to a node as a child of the node.
+
+        First find all the ancestors of a node.
+
+        Find all edges connected to node in reverse.
+
+        Return list of nodes and tuples, with conclusion node as first element and a list as second element.
+
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         list_of_nodes = []
         list_of_edges = []
     
@@ -210,6 +379,19 @@ class Centrality:
 
     @staticmethod
     def get_all_edges(graph):
+         """
+        Function to get all edges connected to a node in both directions from the node.
+
+        First find all the ancestors of a node.
+
+        Find all edges connected to node using in and out edge functions.
+
+        Return list of nodes and tuples, with conclusion node as first element and a list as second element.
+
+        Parameters
+        ----------
+        graph: aif_graph for corpus
+        """
         list_of_nodes = []
         list_of_edges = []
 
--- app/load_map.py
+++ app/load_map.py
@@ -6,9 +6,21 @@ import networkx as nx
 
 
 class CorpusLoader:
+     """
+        Class to take a corpus or nodeset id and load it into a networkx graph
 
+
+    """
     @staticmethod
     def parse_timestamp(timestamp):
+        """
+        Function to parse a timestamp from a node.
+
+
+        Parameters
+        ----------
+        timestamp: timestamp of node in %Y-%m-%d %H:%M:%S datetime format
+        """
         try:
             cast_timestamp = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
         except (ValueError, TypeError):
@@ -19,6 +31,14 @@ class CorpusLoader:
 
     @staticmethod
     def parse_scheme_id(scheme_id):
+        """
+        Function to parse the scheme ID.
+
+
+        Parameters
+        ----------
+        scheme_id: str
+        """
         try:
             cast_scheme_id = int(scheme_id)
         except (ValueError, TypeError):
@@ -29,6 +49,14 @@ class CorpusLoader:
 
     @staticmethod
     def parse_node_id(node_id):
+        """
+        Function to parse node ID as an Int.
+
+
+        Parameters
+        ----------
+        node_id: str
+        """
         try:
             cast_node_id = int(node_id)
         except (ValueError, TypeError):
@@ -39,6 +67,14 @@ class CorpusLoader:
 
     @staticmethod
     def parse_edge_id(edge_id):
+        """
+        Function to parse edge ID as an Int.
+
+
+        Parameters
+        ----------
+        edge_id: str
+        """
         try:
             case_edge_id = int(edge_id)
         except (ValueError, TypeError):
@@ -48,6 +84,14 @@ class CorpusLoader:
         return case_edge_id
 
     def load_corpus(self, directory_path):
+        """
+        Function to load a corpus or nodeset and extract the json data
+
+
+        Parameters
+        ----------
+        directory_path: str
+        """
         directory_path = Path(directory_path)
         json_files = directory_path.rglob('*.json')
         node_sets = {}
@@ -68,7 +112,15 @@ class CorpusLoader:
         
 
     def parse_json(self, node_set):
+        """
+        Function to create a graph from the AIF json
 
+        Creates nodes and edges in a graph based on the node type.
+
+        Parameters
+        ----------
+        node_set: list - containing node_set ids and JSON data
+        """
         G = nx.DiGraph()
         locution_dict = {}
 
